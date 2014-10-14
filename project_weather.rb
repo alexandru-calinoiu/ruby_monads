506dd569ecde7972810af2da4a09458c6a1fa95b
calin <calinoiu.alexandru@agilefreaks.com>
2014-10-14 13:12:02 +0300

    project weather happy path

---
+++ project_weather.rb
@@ -0,0 +1,17 @@
+Project = Struct.new(:creator)
+Creator = Struct.new(:address)
+Address = Struct.new(:country)
+Country = Struct.new(:capital)
+City = Struct.new(:weather)
+
+def weather_for_project(project)
+	project.creator.address.country.capital.weather
+end
+
+# passing
+city = City.new('cilly')
+country = Country.new(city)
+address = Address.new(country)
+creator = Creator.new(address)
+project = Project.new(creator)
+p weather_for_project(project)
